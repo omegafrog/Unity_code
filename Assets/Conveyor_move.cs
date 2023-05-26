06ece5e6fda5f4e2052827ed9af89731beb2f475
yuuu9873 <uijin9873@gmail.com>
2023-05-26 16:42:31 +0900

    remove static

--- Assets/Conveyor_move.cs
+++ Assets/Conveyor_move.cs
@@ -6,7 +6,7 @@ public class Conveyor_move : MonoBehaviour
 {
     // Start is called before the first frame update
     Renderer sr;
-    public static GameObject ob;
+    public GameObject ob;
     
     
     public float object_speed;
